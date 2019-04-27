e71155afde1a4179ec3a229430be85d3f359e368
xyzen <xyzentw@gmail.com>
2019-04-26 22:05:28 -0500

    Class hierarchy

--- Source/src/Controller/Controller.java
+++ Source/src/Controller/Controller.java
@@ -10,5 +10,11 @@ package Controller;
  * @author Tyler
  */
 public class Controller {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
