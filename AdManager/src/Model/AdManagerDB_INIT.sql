a3b38eb5e48e8e85c97fdf02dc51c2c1ed240966
xyzen <xyzentw@gmail.com>
2019-04-29 15:45:21 -0500

    Populated tables

--- AdManager/src/Model/AdManagerDB_INIT.sql
+++ AdManager/src/Model/AdManagerDB_INIT.sql
@@ -35,9 +35,9 @@ CREATE TABLE IF NOT EXISTS Advertisements
 AdvTitle tinytext not null,
 AdvDetails tinytext not null,
 AdvDateTime DATETIME not null default now(),
-Price integer not null,
+Price decimal(15,2) not null,
 User_ID smallint unsigned not null,
-Moderator_ID smallint unsigned not null,
+Moderator_ID smallint unsigned,
 Category_ID varchar(3) not null,
 Status_ID varchar(2) not null,
 constraint pk_advertisements primary key(User_ID),
@@ -61,15 +61,14 @@ insert into Categories(Category_ID, CatName)
 values ('CAT', 'Cars and Trucks'), ('HOU', 'Housing'),
 ('ELC', 'Electronics'), ('CCA', 'Child Care');
 
-insert into Users(UrsFirst_Name,UrsLast_Name)
-values ('John','Smith'),('Ann','Jackson'),('Rania','Kale'),('Samir','Ali');
+insert into Users(User_ID, UrsFirst_Name,UrsLast_Name)
+values (0, 'John','Smith'),(1, 'Ann','Jackson'),(2, 'Rania','Kale'),(3, 'Samir','Ali');
 
 insert into Moderators(User_ID)
 values ('0'),('1');
 
 insert into Advertisements(AdvTitle, AdvDateTime, Price, Category_ID, User_ID, Moderator_ID, Status_ID)
 values ('2010 Sedan Subaru', '2010 sedan car in great shape for sale',  DATETIME('2017-02-10'), 6000, 'CAT', 2, 1, 'AC'),
-('Nice Office Desk','',  DATETIME(''), , '', , , ''),
-('','',  DATETIME(''), , '', , , ''),
-('','',  DATETIME(''), , '', , , '');
-
+('Nice Office Desk','Nice office desk for sale',  DATETIME('2017-02-15'), 50.25, 'HOU', 3, 0, 'AC'),
+('Smart LG TV for $200 ONLY','Smart LG TV 52 inches! Really cheap!',  DATETIME('2017-03-15'), 200, 'ELC', 2, NULL, 'PN'),
+('HD Tablet for $25 only','Amazon Fire Tablet HD',  DATETIME('2017-03-20'), 25, 'ELC', 3, NULL, 'PN');
