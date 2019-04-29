606d39abbbe802929d5eeafbbded783cb280951f
xyzen <xyzentw@gmail.com>
2019-04-29 16:07:40 -0500

    Populated tables

--- AdManager/src/Model/AdManagerDB_INIT.sql
+++ AdManager/src/Model/AdManagerDB_INIT.sql
@@ -17,7 +17,7 @@ constraint pk_statuses primary key(Status_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Users
-(User_ID smallint unsigned not null auto_increment,
+(User_ID smallint unsigned not null,
 UsrFirst_Name varchar(20) not null,
 UsrLast_Name varchar(20) not null,
 constraint pk_users primary key(User_ID)
@@ -40,7 +40,7 @@ User_ID smallint unsigned not null,
 Moderator_ID smallint unsigned,
 Category_ID varchar(3) not null,
 Status_ID varchar(2) not null,
-constraint pk_advertisements primary key(User_ID),
+constraint pk_advertisements primary key(Advertisement_ID),
 constraint fk_advertisements_users foreign key(User_ID)
 references Users (User_ID) ON DELETE CASCADE,
 constraint fk_advertisements_moderators foreign key(Moderator_ID)
@@ -61,14 +61,14 @@ insert into Categories(Category_ID, CatName)
 values ('CAT', 'Cars and Trucks'), ('HOU', 'Housing'),
 ('ELC', 'Electronics'), ('CCA', 'Child Care');
 
-insert into Users(User_ID, UrsFirst_Name,UrsLast_Name)
-values (0, 'John','Smith'),(1, 'Ann','Jackson'),(2, 'Rania','Kale'),(3, 'Samir','Ali');
+insert into Users(User_ID, UsrFirst_Name,UsrLast_Name)
+values (1, 'John','Smith'),(2, 'Ann','Jackson'),(3, 'Rania','Kale'),(4, 'Samir','Ali');
 
 insert into Moderators(User_ID)
-values ('0'),('1');
+values (1),(2);
 
-insert into Advertisements(AdvTitle, AdvDateTime, Price, Category_ID, User_ID, Moderator_ID, Status_ID)
-values ('2010 Sedan Subaru', '2010 sedan car in great shape for sale',  DATETIME('2017-02-10'), 6000, 'CAT', 2, 1, 'AC'),
-('Nice Office Desk','Nice office desk for sale',  DATETIME('2017-02-15'), 50.25, 'HOU', 3, 0, 'AC'),
-('Smart LG TV for $200 ONLY','Smart LG TV 52 inches! Really cheap!',  DATETIME('2017-03-15'), 200, 'ELC', 2, NULL, 'PN'),
-('HD Tablet for $25 only','Amazon Fire Tablet HD',  DATETIME('2017-03-20'), 25, 'ELC', 3, NULL, 'PN');
+insert into Advertisements(AdvTitle, AdvDetails, AdvDateTime, Price, Category_ID, User_ID, Moderator_ID, Status_ID)
+values ('2010 Sedan Subaru', '2010 sedan car in great shape for sale', '2017-02-10', 6000, 'CAT', 3, 1, 'AC'),
+('Nice Office Desk', 'Nice office desk for sale', '2017-02-15', 50.25, 'HOU', 4, 2, 'AC'),
+('Smart LG TV for $200 ONLY','Smart LG TV 52 inches! Really cheap!', '2017-03-15', 200, 'ELC', 3, NULL, 'PN'),
+('HD Tablet for $25 only','Amazon Fire Tablet HD', '2017-03-20', 25, 'ELC', 4, NULL, 'PN');
