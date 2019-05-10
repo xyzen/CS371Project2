21add677865785dc9b9f8af1e1bbd0cb6e6afa56
xyzen <xyzentw@gmail.com>
2019-05-09 19:32:42 -0500

    Controller

--- AdManager/src/Controller/Controller.java
+++ AdManager/src/Controller/Controller.java
@@ -12,15 +12,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.LinkedList;
 import java.util.Properties;
 import java.util.Date;
-import View.UserView;
 import View.EditView;
 import View.AddView;
-import View.ModView;
 import View.LoginView;
-
 
 
 public class Controller {
@@ -29,23 +25,25 @@ public class Controller {
      */
     
     private Connection connection;
-    private LoginView lv;
-    private UserView uv;
+    private final LoginView lv;
     private AddView av;
     private EditView ev;
-    private ModView mv;
     
     public Controller() {
         lv = new LoginView(this);
     }
     
     public void start() {
-        lv.setVisibility(true);
+        lv.setVisible(true);
+    }
+    
+    public void handleLoginRequest(String username, String userType) {
+        
     }
    
     public void handleAddAdvertisement(String title, String desc, String cat, float price) {
         av = new AddView();
-        av.setVisibility(true);
+        av.setVisible(true);
     }
     //The button pushed tells the controller to create and set the view to be visible.
     public void handleAddAdButtonPushed(){
@@ -56,16 +54,15 @@ public class Controller {
     
     public void handleEditAdvertisement(int adv_id) {
         ev = new EditView(adv_id);
-        ev.setVisibility(true);
+        ev.setVisible(true);
     }
     
     public void handleDeleteAdvertisement(int adv_id) {
-        dv = new DeleteView();
-        dv.setVisibility(true);// does not bring up a page only updates the database
+        
     }
     
     public void handleUserSTDTableRequest(String category, int months, String keyword){
-        PreparedStatement stmt = null;
+        PreparedStatement stmt;
         Date date = new Date();
         date.setMonth(date.getMonth() - months);
         String date_arg = Integer.toString(date.getYear())+"-"+Integer.toString(date.getMonth())+"-"+Integer.toString(date.getDate());
@@ -107,16 +104,6 @@ public class Controller {
         }
         return count;
     }
-    public static void main(String[] args) {
-        Controller c = new Controller();
-        try {
-            c.connect("user", "1234", "localhost", "3306", "project2");
-        } catch(SQLException | InstantiationException | IllegalAccessException e) {}
-        //There is still a remnant of the View Class in this file, we need to change this to likely the instantiate the controller
-        // or the LoginView
-        
-        View v = new View(c);
-    }
     
     public void connect(String userName, String password, String serverName, String portNumber, String dbName) throws SQLException, InstantiationException, IllegalAccessException {
         System.out.println("Loading driver...");
@@ -128,7 +115,7 @@ public class Controller {
             throw new IllegalStateException("Cannot find the driver in the classpath!", e);
         }
 
-        Connection conn = null;
+        Connection conn;
         Properties connectionProps = new Properties();
         connectionProps.put("user", userName);
         connectionProps.put("password", password);
--- AdManager/src/View/LoginView.java
+++ AdManager/src/View/LoginView.java
@@ -19,8 +19,9 @@ public class LoginView extends javax.swing.JFrame {
     /**
      * Creates new form LoginView
      */
-    public LoginView() {
+    public LoginView(Controller c) {
         initComponents();
+        master = c;
     }
 
     /**
@@ -97,40 +98,6 @@ public class LoginView extends javax.swing.JFrame {
         
     }//GEN-LAST:event_loginButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoginView().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton loginButton;
