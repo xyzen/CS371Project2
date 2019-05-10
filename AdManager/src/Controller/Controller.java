d4db65c96ebaacdc4a4ef19dc066d32d831128e7
xyzen <xyzentw@gmail.com>
2019-05-10 00:12:57 -0500

    UI

--- AdManager/src/Controller/Controller.java
+++ AdManager/src/Controller/Controller.java
@@ -35,10 +35,63 @@ public class Controller {
     public void start() {
         lv.setVisible(true);
     }
+        
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        Controller c = new Controller();
+        try {
+            c.connect("user", "1234", "localhost", "3306", "project2");
+            c.start();
+        } catch (SQLException | InstantiationException | IllegalAccessException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Could not connect to database.");
+        }
+    }
     
+    public void connect(String userName, String password, String serverName, String portNumber, String dbName) throws SQLException, InstantiationException, IllegalAccessException {
+        System.out.println("Loading driver...");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+
+        Connection conn;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", userName);
+        connectionProps.put("password", password);
+
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://"
+                + serverName
+                + ":" + portNumber + "/" + dbName,
+                connectionProps);
+        System.out.println("Connected to database");
+        this.connection=conn;
+    }
+    
+    private int getResultSetSize(ResultSet rs) {
+        int count = 0;
+        try {
+            while (rs.next()) {
+                count++;
+            }
+            rs.first();
+        } catch (SQLException e) {
+
+        }
+        return count;
+    }
+        
     public void handleLoginRequest(String username, String userType) {
         PreparedStatement stmt = null;
-        int size; String user_id; ResultSet rs;
+        String user_id;
+        ResultSet rs;
+        int size;
         String query = "SELECT User_ID FROM Users WHERE ";
         switch(userType) {
             
@@ -79,7 +132,74 @@ public class Controller {
                 break;
         }
     }
-   
+    
+    public void handleUserSTDTableRequest(String category, int months, String keyword){
+        PreparedStatement stmt = null;
+        Date date = new Date();
+        date.setMonth(date.getMonth() - months);
+        String date_arg = Integer.toString(date.getYear())+"-"+Integer.toString(date.getMonth())+"-"+Integer.toString(date.getDate());
+        String query = "SELECT AdvTitle, AdvDetails, Price, AdvDateTime"
+                + "FROM Advertisements"
+                + "WHERE Status_ID='AC' AND Category_ID='?' AND (AdvTitle LIKE '?' OR A.AdvDetails LIKE '?')";
+        if (months > 0) {
+            query += "AND AdvDateTime<'?'";
+        }
+        query += ";";
+        try {
+            stmt=connection.prepareStatement(query);
+            stmt.setString(1, category);
+            stmt.setString(2, date_arg);
+            stmt.setString(3, "%" + keyword + "%");
+            stmt.setString(4, "%" + keyword + "%");
+            ResultSet rs = stmt.executeQuery();
+            int count = getResultSetSize(rs);
+            String[][] published_data = new String[count][4];
+            int index = 0;
+            while(rs.next()){
+                published_data[index][0] = rs.getString("AdvTitle");
+                published_data[index][1] = rs.getString("AdvDetails");
+                published_data[index][2] = rs.getString("Price");
+                published_data[index][3] = rs.getString("AdvDateTime");
+                index++;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void handleUserMyTableRequest(String userID){
+        PreparedStatement stmt = null;
+        String query = "SELECT AdvTitle, AdvDetails, Price, AdvDateTime"
+                + "FROM Advertisements"
+                + "WHERE Status_ID='AC' AND Category_ID='?' AND AdvDateTime<'?' AND (AdvTitle LIKE '?' OR A.AdvDetails LIKE '?');";
+        try {
+            stmt=connection.prepareStatement(query);
+            stmt.setString(1, category);
+            stmt.setString(2, date_arg);
+            stmt.setString(3, "%" + keyword + "%");
+            stmt.setString(4, "%" + keyword + "%");
+            ResultSet rs = stmt.executeQuery();
+            int count = getResultSetSize(rs);
+            String[][] published_data = new String[count][4];
+            int index = 0;
+            while(rs.next()){
+                published_data[index][0] = rs.getString("AdvTitle");
+                published_data[index][1] = rs.getString("AdvDetails");
+                published_data[index][2] = rs.getString("Price");
+                published_data[index][3] = rs.getString("AdvDateTime");
+                index++;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    //The button pushed tells the controller to create and set the view to be visible.
+    public void handleAddButton(String userID){
+        av = new AddView(this, userID);
+        av.setVisible(true);
+    }
+    
     public void handleAddRequest(String title, String desc, String cat, String price, String user_id) {
         switch (cat) {
             case "Housing":
@@ -116,12 +236,6 @@ public class Controller {
         }
     }
     
-    //The button pushed tells the controller to create and set the view to be visible.
-    public void handleAddButton(String userID){
-        av = new AddView(this, userID);
-        av.setVisible(true);
-    }
-    
     public void handleEditButton(String adv_id, String user_id) {
         ev = new EditView(this, adv_id, user_id);
         ev.setVisible(true);
@@ -136,124 +250,19 @@ public class Controller {
         
     }
     
+    public void handleModSTDTableRequest(String category, int months, String keyword) {
+        
+    }
+    
+    public void handleModMyTableRequest(String userID) {
+        
+    }
+    
     public void handleApproveRequest(String advID, String userID) {
         
     }
     
     public void handleClaimRequest(String advID, String userID) {
         
-    }
-    
-    public void handleModSTDTableRequest(String category, int months, String keyword) {
-        
-    }
-    
-    public void handleUserSTDTableRequest(String category, int months, String keyword){
-        PreparedStatement stmt = null;
-        Date date = new Date();
-        date.setMonth(date.getMonth() - months);
-        String date_arg = Integer.toString(date.getYear())+"-"+Integer.toString(date.getMonth())+"-"+Integer.toString(date.getDate());
-        String query = "SELECT AdvTitle, AdvDetails, Price, AdvDateTime"
-                + "FROM Advertisements"
-                + "WHERE Status_ID='AC' AND Category_ID='?' AND (AdvTitle LIKE '?' OR A.AdvDetails LIKE '?')";
-        if (months > 0) {
-            query += "AND AdvDateTime<'?'";
-        }
-        query += ";";
-        try {
-            stmt=connection.prepareStatement(query);
-            stmt.setString(1, category);
-            stmt.setString(2, date_arg);
-            stmt.setString(3, "%" + keyword + "%");
-            stmt.setString(4, "%" + keyword + "%");
-            ResultSet rs = stmt.executeQuery();
-            int count = getResultSetSize(rs);
-            String[][] published_data = new String[count][4];
-            int index = 0;
-            while(rs.next()){
-                published_data[index][0] = rs.getString("AdvTitle");
-                published_data[index][1] = rs.getString("AdvDetails");
-                published_data[index][2] = rs.getString("Price");
-                published_data[index][3] = rs.getString("AdvDateTime");
-                index++;
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-        
-    public void handleUserMyTableRequest(String category, int months, String keyword){
-        PreparedStatement stmt = null;
-        Date date = new Date();
-        date.setMonth(date.getMonth() - months);
-        String date_arg = Integer.toString(date.getYear())+"-"+Integer.toString(date.getMonth())+"-"+Integer.toString(date.getDate());
-        String query = "SELECT AdvTitle, AdvDetails, Price, AdvDateTime"
-                + "FROM Advertisements"
-                + "WHERE Status_ID='AC' AND Category_ID='?' AND AdvDateTime<'?' AND (AdvTitle LIKE '?' OR A.AdvDetails LIKE '?');";
-        try {
-            stmt=connection.prepareStatement(query);
-            stmt.setString(1, category);
-            stmt.setString(2, date_arg);
-            stmt.setString(3, "%" + keyword + "%");
-            stmt.setString(4, "%" + keyword + "%");
-            ResultSet rs = stmt.executeQuery();
-            int count = getResultSetSize(rs);
-            String[][] published_data = new String[count][4];
-            int index = 0;
-            while(rs.next()){
-                published_data[index][0] = rs.getString("AdvTitle");
-                published_data[index][1] = rs.getString("AdvDetails");
-                published_data[index][2] = rs.getString("Price");
-                published_data[index][3] = rs.getString("AdvDateTime");
-                index++;
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    
-      private int getResultSetSize(ResultSet rs) {
-        int count = 0;
-        try {
-            while (rs.next()) {
-                count++;
-            }
-            rs.first();
-        } catch (SQLException e) {
-
-        }
-        return count;
-    }
-    
-    public void connect(String userName, String password, String serverName, String portNumber, String dbName) throws SQLException, InstantiationException, IllegalAccessException {
-        System.out.println("Loading driver...");
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            System.out.println("Driver loaded!");
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
-        }
-
-        Connection conn;
-        Properties connectionProps = new Properties();
-        connectionProps.put("user", userName);
-        connectionProps.put("password", password);
-
-        conn = DriverManager.getConnection(
-                "jdbc:mysql://"
-                + serverName
-                + ":" + portNumber + "/" + dbName,
-                connectionProps);
-        System.out.println("Connected to database");
-        this.connection=conn;
-    }
-    
-    /**
-    * @param args
-    */
-    public static void main(String[] args) {
-        Controller c = new Controller();
-        c.start();
     }
 }
