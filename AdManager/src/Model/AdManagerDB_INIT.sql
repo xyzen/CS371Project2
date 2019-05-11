c76be8403b3a377a10d1e8620f889fa5ad8c61a0
Christopher Swayne <chris_swa@icloud.com>
2019-05-10 22:23:21 -0500

    Added more records to the SQL script
    We needed more records for testing with data.

--- AdManager/src/Model/AdManagerDB_INIT.sql
+++ AdManager/src/Model/AdManagerDB_INIT.sql
@@ -74,4 +74,13 @@ insert into Advertisements(AdvTitle, AdvDetails, AdvDateTime, Price, Category_ID
 values ('2010 Sedan Subaru', '2010 sedan car in great shape for sale', '2017-02-10', 6000, 'CAT', 3, 1, 'AC'),
 ('Nice Office Desk', 'Nice office desk for sale', '2017-02-15', 50.25, 'HOU', 4, 2, 'AC'),
 ('Smart LG TV for $200 ONLY','Smart LG TV 52 inches! Really cheap!', '2017-03-15', 200, 'ELC', 3, NULL, 'PN'),
-('HD Tablet for $25 only','Amazon Fire Tablet HD', '2017-03-20', 25, 'ELC', 4, NULL, 'PN');
+('HD Tablet for $25 only','Amazon Fire Tablet HD', '2017-03-20', 25, 'ELC', 4, NULL, 'PN'),
+('2005 Nissan Altima', 'Good used car for sale', '2019-04-15', 4500, 'CAT', 3, 2, 'DI'),
+('1999 Isuzu Rodeo', 'Great SUV in great shape for sale', '2018-10-30', 2000,'CAT', 4, 1, 'AC'),
+('Kenwood Stereo', 'Automotive radio unit for sale', '2019-01-01', 20, 'ELC', 3, NULL, 'PN'),
+('Hut', 'Nice hut just on the lake front!!', '2019-02-10', 399, 'HOU', 3, 1, 'AC'),
+('Java Smasher', 'This handy device takes all your problems away', '2019-05-10', 70, 'ELC', 4, 2, 'PN'),
+('Babby Sitter','Can sit your kids whenever you need', '2018-11-15', 20, 'CCA', 4, 1, 'AC'),
+('Tonka Truck','Still in the packaging', '2019-03-20', 80, 'CAT', 2, 1, 'AC'),
+('2011 MacBook Pro', 'I know what I have, no low ballers', '2018-11-14', 314 , 'ELC', 3, NULL, 'PN' ),
+('2013 Windows Desktop', 'Custom Built', '2019-05-09', 1000, 'ELC', 3, 1, 'DI' );
